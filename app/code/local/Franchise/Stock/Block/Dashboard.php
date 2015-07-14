2846a8e3784d1cd91286074fa2c1f5a631b23ef3
Ronildo dos Santos <ronildo_net@hotmail.com>
2015-07-14 11:00:01 -0300

    Mesagem inclusion to blank data

--- app/code/local/Franchise/Stock/Block/Dashboard.php
+++ app/code/local/Franchise/Stock/Block/Dashboard.php
@@ -29,6 +29,16 @@
 					$this->setData('productMoreProfit', $dashboard->getProductMoreProfit($dashboard::INTERVAL_TODAY));
 					$this->setData('fullPurchases', $dashboard->getFullPurchases($dashboard::INTERVAL_TODAY));
 					$this->setData('generateMoreCommision', $dashboard->getGenerateMoreCommission($dashboard::INTERVAL_TODAY));
+
+					$arrComboBoxSelected = array(
+						0 => '',
+						1 => 'selected="selected"',
+						7 => '',
+						30 => '',
+					);
+
+					$this->setData('comboBoxSelected', $arrComboBoxSelected);
+
 					break;
 
 				case 7:
@@ -38,6 +48,16 @@
 					$this->setData('productMoreProfit', $dashboard->getProductMoreProfit($dashboard::INTERVAL_SEVEN_DAYS));
 					$this->setData('fullPurchases', $dashboard->getFullPurchases($dashboard::INTERVAL_SEVEN_DAYS));
 					$this->setData('generateMoreCommision', $dashboard->getGenerateMoreCommission($dashboard::INTERVAL_SEVEN_DAYS));
+
+					$arrComboBoxSelected = array(
+						0 => '',
+						1 => '',
+						7 => 'selected="selected"',
+						30 => '',
+					);
+
+					$this->setData('comboBoxSelected', $arrComboBoxSelected);
+
 					break;
 
 				case 30:
@@ -47,6 +67,15 @@
 					$this->setData('productMoreProfit', $dashboard->getProductMoreProfit($dashboard::INTERVAL_THIRTY_DAYS));
 					$this->setData('fullPurchases', $dashboard->getFullPurchases($dashboard::INTERVAL_THIRTY_DAYS));
 					$this->setData('generateMoreCommision', $dashboard->getGenerateMoreCommission($dashboard::INTERVAL_THIRTY_DAYS));
+
+					$arrComboBoxSelected = array(
+						0 => '',
+						1 => '',
+						7 => '',
+						30 => 'selected="selected"',
+					);
+
+					$this->setData('comboBoxSelected', $arrComboBoxSelected);
 					break;
 
 				default:
@@ -56,6 +85,15 @@
 					$this->setData('productMoreProfit', $dashboard->getProductMoreProfit());
 					$this->setData('fullPurchases', $dashboard->getFullPurchases());
 					$this->setData('generateMoreCommision', $dashboard->getGenerateMoreCommission());
+
+					$arrComboBoxSelected = array(
+						0 => 'selected="selected"',
+						1 => '',
+						7 => '',
+						30 => '',
+					);
+
+					$this->setData('comboBoxSelected', $arrComboBoxSelected);
 					break;
 			}
 		}
