66deac8ee7f574603d40cc3e06fa69f9a404fc7e
Ronildo dos Santos <ronildo_net@hotmail.com>
2015-07-27 15:53:04 -0300

    Creation of FAQ page

---
+++ app/code/local/Help/Customer/Block/Faq.php
@@ -0,0 +1,17 @@
+<?php
+
+class Help_Customer_Block_Faq extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        return $this;
+    }
+}
