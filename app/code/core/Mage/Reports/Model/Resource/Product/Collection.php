646d26e0fcaa85f4e967a3791c39c9e604670536
Ronildo dos Santos <ronildo_net@hotmail.com>
2015-09-04 16:02:00 -0300

    Includes the report billing by period

--- app/code/core/Mage/Reports/Model/Resource/Product/Collection.php
+++ app/code/core/Mage/Reports/Model/Resource/Product/Collection.php
@@ -168,7 +168,6 @@ class Mage_Reports_Model_Resource_Product_Collection extends Mage_Catalog_Model_
         $this->addAttributeToSelect('entity_id')
             ->addAttributeToSelect('name')
             ->addAttributeToSelect('price');
-
         return $this;
     }
 
