dac7d071785fe3daa1983bd4d39b29b7259b9c0a
Ronildo dos Santos <ronildo_net@hotmail.com>
2015-09-24 14:19:06 -0300

    Adds commercial reference fields in the new user registration.

--- app/code/local/Franchise/Group/sql/group_setup/mysql4-install-0.1.1.php
+++ app/code/local/Franchise/Group/sql/group_setup/mysql4-install-0.1.1.php
@@ -5,23 +5,89 @@ $installer = $this;
 $installer->startSetup();
 
 $setup = Mage::getModel('customer/entity_setup', 'core_setup');
-$setup->addAttribute('customer', 'franchiseoption', array(
-        'type' => 'int',
-        'input' => 'select',
-        'label' => 'Opção Franquia',
+
+$setup->addAttribute('customer', 'referenceone', array(
+
+        'type' => 'varchar',
+        'input' => 'text',
+        'label' => 'Referência 1',
         'global' => 1,
         'visible' => 1,
         'required' => 0,
         'user_defined' => 1,
         'default' => '0',
         'visible_on_front' => 1,
-        'source' => 'group/entity_franchiseoption',
+));
+
+
+
+$setup->addAttribute('customer', 'referencetwo', array(
+
+        'type' => 'varchar',
+        'input' => 'text',
+        'label' => 'Referência 2',
+        'global' => 1,
+        'visible' => 1,
+        'required' => 0,
+        'user_defined' => 1,
+        'default' => '0',
+        'visible_on_front' => 1,
+));
+
+
+
+$setup->addAttribute('customer', 'phoneone', array(
+
+        'type' => 'varchar',
+        'input' => 'text',
+        'label' => 'Telefone 1',
+        'global' => 1,
+        'visible' => 1,
+        'required' => 0,
+        'user_defined' => 1,
+        'default' => '0',
+        'visible_on_front' => 1,
+));
+
+
+
+$setup->addAttribute('customer', 'phonetwo', array(
+
+        'type' => 'varchar',
+        'input' => 'text',
+        'label' => 'Telefone 2',
+        'global' => 1,
+        'visible' => 1,
+        'required' => 0,
+        'user_defined' => 1,
+        'default' => '0',
+        'visible_on_front' => 1,
 ));
 
 if (version_compare(Mage::getVersion(), '1.4.2', '>='))
 {
+
     Mage::getSingleton('eav/config')
-            ->getAttribute('customer', 'franchiseoption')
+            ->getAttribute('customer', 'referenceone')
+            ->setData('used_in_forms', array('adminhtml_customer', 'customer_account_create'))
+            ->save();
+
+
+
+    Mage::getSingleton('eav/config')
+            ->getAttribute('customer', 'referencetwo')
+            ->setData('used_in_forms', array('adminhtml_customer', 'customer_account_create'))
+            ->save();
+
+    Mage::getSingleton('eav/config')
+            ->getAttribute('customer', 'phoneone')
+            ->setData('used_in_forms', array('adminhtml_customer', 'customer_account_create'))
+            ->save();
+
+
+
+    Mage::getSingleton('eav/config')
+            ->getAttribute('customer', 'phonetwo')
             ->setData('used_in_forms', array('adminhtml_customer', 'customer_account_create'))
             ->save();
 
--- app/code/local/Wizard/Customer/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Wizard_Customer_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
