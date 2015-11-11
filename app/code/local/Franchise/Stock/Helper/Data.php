788818f76525f75d1730c2f5e7e871205c926ab5
ZiruZanrgeiff <ronildo_net@hotmail.com>
2015-11-11 16:27:19 -0200

    Fix bung in financialchart report

--- app/code/local/Franchise/Stock/Helper/Data.php
+++ app/code/local/Franchise/Stock/Helper/Data.php
@@ -1,5 +1,23 @@
 <?PHP
 class Franchise_Stock_Helper_Data extends Mage_Core_Helper_Abstract {
 
+    private $commissionSubmited = false;
+
+    /**
+     * Checks whether a search tab commission was submitted.
+     *
+     * @return boolean
+     */
+    public function isCommissionSubmited()
+    {
+        return $this->commissionSubmited;
+    }
+
+    /**
+     * @param boolean $commissionSubmited
+     */
+    public function setCommissionSubmited( $commissionSubmited )
+    {
+        $this->commissionSubmited = $commissionSubmited;
+    }
 }
-?>
