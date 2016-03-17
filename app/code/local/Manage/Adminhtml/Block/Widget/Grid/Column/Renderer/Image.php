48d071ab6ea5570f7c129e51f5c81a99c5a40f28
Ziru Zanrgeiff <ronildo_net@hotmail.com>
2016-03-17 15:46:37 -0300

    Recover image product by product configurable

--- app/code/local/Manage/Adminhtml/Block/Widget/Grid/Column/Renderer/Image.php
+++ app/code/local/Manage/Adminhtml/Block/Widget/Grid/Column/Renderer/Image.php
@@ -1,20 +1,40 @@
 <?php
 
+/**
+ * This class provides a way to show images products.
+ * The image configurable product is show in all your child.
+ * The configurable product is recovered by child id.
+ *
+ * @category   Manage
+ * @package    Manage_Adminhtml
+ * @author     Ronildo dos Santos - Planeta Futebol Developer Team
+ */
 class Manage_Adminhtml_Block_Widget_Grid_Column_Renderer_Image
     extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
-    public function render(Varien_Object $row)
+    public function render( Varien_Object $row )
     {
         $productId =  $row->getData($this->getColumn()->getIndex());
-        $product = Mage::getModel('catalog/product')->load($productId);
+
+        // recover the configurable id product by your child id
+        $parentIds = Mage::getResourceSingleton('catalog/product_type_configurable')
+            ->getParentIdsByChild($productId);
+
+        $product = Mage::getModel('catalog/product')->load($parentIds);
+
+        $productMediaConfig = Mage::getModel('catalog/product_media_config');
+
+        // absolute path to image product
+        $baseImageUrl = $productMediaConfig->getMediaUrl($product->getImage());
 
         $value = 'Sem imagem';
+
         if($product->getImage() && $product->getImage() != 'no_selection')
         {
-            $value='<img src="' . $product->getImageUrl() . '" width="100" height="100" />';
+            $value ='<img src="' . $baseImageUrl . '" width="100" height="100" />';
+
         }
 
         return $value;
-
     }
 }
