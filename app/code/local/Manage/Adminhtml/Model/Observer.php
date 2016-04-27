535f0c1b32511f70aafe1cea9f41a694114bec03
Ziru Zanrgeiff <ronildo_net@hotmail.com>
2016-04-27 10:28:45 -0300

    Comment observers, to prevent fix in import sales.

--- app/code/local/Manage/Adminhtml/Model/Observer.php
+++ app/code/local/Manage/Adminhtml/Model/Observer.php
@@ -17,93 +17,93 @@ class Manage_Adminhtml_Model_Observer
      */
     public function getCustomerGrid( Varien_Event_Observer $observer )
     {
-        $block = $observer->getEvent()->getBlock();
-
-        if ($block->getId() == 'customerGrid') {
-
-            $block->removeColumn('taxvat');
-            $block->removeColumn('gender');
-            $block->removeColumn('dob');
-            $block->removeColumn('age');
-            $block->removeColumn('billing_postcode');
-
-            $block->addColumn( 'account_name', array(
-                    'header'    => Mage::helper('customer')->__('Nome do Representante'),
-                    'filter'    => false,
-                    'index'     => 'account_name'
-                )
-            );
-
-            $block->addColumn('billing_city',[
-                'header'    => Mage::helper('customer')->__('City'),
-                'width'     => '100',
-                'type'      => 'city',
-                'filter'    => false,
-                'index'     => 'billing_city',
-            ]);
-
-            $block->addColumn('total_purchases',[
-                'header'    => Mage::helper('customer')->__('Total de Compras'),
-                'width'     => '100',
-                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
-                'filter'    => false,
-                'index'     => 'total_purchases',
-            ]);
-
-            $block->addColumn('media_purchases',[
-                'header'    => Mage::helper('customer')->__('Média de Compras/Mês'),
-                'width'     => '100',
-                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
-                'filter'    => false,
-                'index'     => 'media_purchases',
-            ]);
-
-            $block->addColumn('qty_purchases',[
-                'header'    => Mage::helper('customer')->__('Qty de Pedidos'),
-                'width'     => '100',
-                'filter'    => false,
-                'index'     => 'qty_purchases',
-            ]);
-
-            $block->addColumn('last_purchases',[
-                'header'    => Mage::helper('customer')->__('Última Compra'),
-                'width'     => '100',
-                'filter'    => false,
-                'index'     => 'last_purchases'
-            ]);
-
-            $block->addColumn('credit_limit',[
-                'header'    => Mage::helper('customer')->__('Limite de Crédito'),
-                'width'     => '100',
-                'filter'    => false,
-                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
-                'index'     => 'credit_limit'
-            ]);
-
-            $block->addColumn('open_value',[
-                'header'    => Mage::helper('customer')->__('Valor em Aberto'),
-                'width'     => '100',
-                'filter'    => false,
-                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
-                'index'     => 'open_value'
-            ]);
-
-            $block->addColumn('has_stande',[
-                'header'    => Mage::helper('customer')->__('Tem Stande'),
-                'width'     => '100',
-                'filter'    => false,
-                'index'     => 'has_stande'
-            ]);
-
-
-            $block->addColumn('payment_range',[
-                'header'    => Mage::helper('customer')->__('Intervalo de Pagamentos'),
-                'width'     => '100',
-                'filter'    => false,
-                'index'     => 'payment_range'
-            ]);
-
-        }
+//        $block = $observer->getEvent()->getBlock();
+//
+//        if ($block->getId() == 'customerGrid') {
+//
+//            $block->removeColumn('taxvat');
+//            $block->removeColumn('gender');
+//            $block->removeColumn('dob');
+//            $block->removeColumn('age');
+//            $block->removeColumn('billing_postcode');
+//
+//            $block->addColumn( 'account_name', array(
+//                    'header'    => Mage::helper('customer')->__('Nome do Representante'),
+//                    'filter'    => false,
+//                    'index'     => 'account_name'
+//                )
+//            );
+//
+//            $block->addColumn('billing_city',[
+//                'header'    => Mage::helper('customer')->__('City'),
+//                'width'     => '100',
+//                'type'      => 'city',
+//                'filter'    => false,
+//                'index'     => 'billing_city',
+//            ]);
+//
+//            $block->addColumn('total_purchases',[
+//                'header'    => Mage::helper('customer')->__('Total de Compras'),
+//                'width'     => '100',
+//                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
+//                'filter'    => false,
+//                'index'     => 'total_purchases',
+//            ]);
+//
+//            $block->addColumn('media_purchases',[
+//                'header'    => Mage::helper('customer')->__('Média de Compras/Mês'),
+//                'width'     => '100',
+//                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
+//                'filter'    => false,
+//                'index'     => 'media_purchases',
+//            ]);
+//
+//            $block->addColumn('qty_purchases',[
+//                'header'    => Mage::helper('customer')->__('Qty de Pedidos'),
+//                'width'     => '100',
+//                'filter'    => false,
+//                'index'     => 'qty_purchases',
+//            ]);
+//
+//            $block->addColumn('last_purchases',[
+//                'header'    => Mage::helper('customer')->__('Última Compra'),
+//                'width'     => '100',
+//                'filter'    => false,
+//                'index'     => 'last_purchases'
+//            ]);
+//
+//            $block->addColumn('credit_limit',[
+//                'header'    => Mage::helper('customer')->__('Limite de Crédito'),
+//                'width'     => '100',
+//                'filter'    => false,
+//                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
+//                'index'     => 'credit_limit'
+//            ]);
+//
+//            $block->addColumn('open_value',[
+//                'header'    => Mage::helper('customer')->__('Valor em Aberto'),
+//                'width'     => '100',
+//                'filter'    => false,
+//                'renderer'  => 'manage_adminhtml_block_widget_grid_column_renderer_money',
+//                'index'     => 'open_value'
+//            ]);
+//
+//            $block->addColumn('has_stande',[
+//                'header'    => Mage::helper('customer')->__('Tem Stande'),
+//                'width'     => '100',
+//                'filter'    => false,
+//                'index'     => 'has_stande'
+//            ]);
+//
+//
+//            $block->addColumn('payment_range',[
+//                'header'    => Mage::helper('customer')->__('Intervalo de Pagamentos'),
+//                'width'     => '100',
+//                'filter'    => false,
+//                'index'     => 'payment_range'
+//            ]);
+//
+//        }
     }
 
     /**
@@ -115,52 +115,52 @@ class Manage_Adminhtml_Model_Observer
      */
     public function beforeCustomerCollectionLoad( Varien_Event_Observer $observer )
     {
-        $collection = $observer->getCollection();
-        if (!isset($collection)) {
-            return;
-        }
-
-        if ( $collection instanceof Mage_Customer_Model_Resource_Customer_Collection ) {
-            /* @var $collection Mage_Customer_Model_Resource_Customer_Collection */
-            $collection->getSelect()
-                ->columns(
-                    [
-                        'total_purchases' => '(
-                            SELECT SUM(grand_total) FROM sales_flat_order
-                            WHERE customer_id = e.entity_id
-                        )',
-
-                        'qty_purchases' => '(
-                            SELECT COUNT(*) FROM sales_flat_order
-                            WHERE customer_id = e.entity_id
-                        )',
-
-                        'media_purchases' => '(
-                            SELECT
-	                            IF(timestampdiff(day, e.created_at, NOW())/30 >= 1,
-	                            ( sum(grand_total) / (( timestampdiff(day, e.created_at, NOW()))/30)),
-	                             sum(grand_total))
-                            FROM sales_flat_order
-                            WHERE customer_id = e.entity_id
-                        )',
-
-                        'last_purchases' => '(
-                            SELECT DATE_FORMAT(created_at,\'%d-%m-%Y\')
-                            FROM sales_flat_order
-                            WHERE customer_id = e.entity_id
-                            ORDER BY created_at DESC
-                            LIMIT 1
-                        )',
-
-                        'account_name' => '(
-                            SELECT TRIM(account_name) FROM affiliateplus_transaction
-                            WHERE customer_id = e.entity_id
-                            LIMIT 1
-                        )',
-
-                        'has_stande' => 'IF((COALESCE(at_has_stande.value, 0) = 0), "Não", "Sim")'
-                    ]
-                );
-        }
+//        $collection = $observer->getCollection();
+//        if (!isset($collection)) {
+//            return;
+//        }
+//
+//        if ( $collection instanceof Mage_Customer_Model_Resource_Customer_Collection ) {
+//            /* @var $collection Mage_Customer_Model_Resource_Customer_Collection */
+//            $collection->getSelect()
+//                ->columns(
+//                    [
+//                        'total_purchases' => '(
+//                            SELECT SUM(grand_total) FROM sales_flat_order
+//                            WHERE customer_id = e.entity_id
+//                        )',
+//
+//                        'qty_purchases' => '(
+//                            SELECT COUNT(*) FROM sales_flat_order
+//                            WHERE customer_id = e.entity_id
+//                        )',
+//
+//                        'media_purchases' => '(
+//                            SELECT
+//	                            IF(timestampdiff(day, e.created_at, NOW())/30 >= 1,
+//	                            ( sum(grand_total) / (( timestampdiff(day, e.created_at, NOW()))/30)),
+//	                             sum(grand_total))
+//                            FROM sales_flat_order
+//                            WHERE customer_id = e.entity_id
+//                        )',
+//
+//                        'last_purchases' => '(
+//                            SELECT DATE_FORMAT(created_at,\'%d-%m-%Y\')
+//                            FROM sales_flat_order
+//                            WHERE customer_id = e.entity_id
+//                            ORDER BY created_at DESC
+//                            LIMIT 1
+//                        )',
+//
+//                        'account_name' => '(
+//                            SELECT TRIM(account_name) FROM affiliateplus_transaction
+//                            WHERE customer_id = e.entity_id
+//                            LIMIT 1
+//                        )',
+//
+//                        'has_stande' => 'IF((COALESCE(at_has_stande.value, 0) = 0), "Não", "Sim")'
+//                    ]
+//                );
+//        }
     }
 }
